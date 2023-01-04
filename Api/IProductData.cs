89c77f02aa8b6573d3f7185cda2cb833dfa06649
JohnMarsing <johnmarsing@gmail.com>
2023-01-04 10:59:50 -0700

    Added a data project and I don't know what else

---
+++ Api/IProductData.cs
@@ -0,0 +1,14 @@
+﻿using Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Api;
+
+public interface IProductData
+{
+	Task<Product> AddProduct(Product product);
+	Task<bool> DeleteProduct(int id);
+	Task<IEnumerable<Product>> GetProducts();
+	Task<Product> UpdateProduct(Product product);
+}
+
---
+++ TorahGatedCommunityDNF6/Pages/Products/LinksProducts.cs
@@ -0,0 +1,19 @@
+﻿namespace TorahGatedCommunityDNF6.Links;
+
+public static partial class LinksProducts
+{
+	public static class List
+	{
+		public const string Index = "/products";
+		public const string Title = "Products";
+		public const string PageTitle = "Product List";
+		public const string Icon = "far fa-list-alt";
+	}
+	public static class Detail
+	{
+		public const string Index = "/product";
+		public const string Title = "Product Detail";
+		public const string PageTitle = "Product Detail";
+		public const string Icon = "fab fa-product-hunt";  
+	}
+}
